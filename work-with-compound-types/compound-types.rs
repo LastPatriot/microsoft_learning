8983d94031fa6f6664fac960bb5b7f564b0ae38a
LastPatriot <oluwadareopeyemis1@gmail.com>
2025-01-20 22:25:46 +0100

    Hash maps

--- work-with-compound-types/compound-types.rs
+++ work-with-compound-types/compound-types.rs
@@ -68,7 +68,11 @@ fn main() {
     car = car_factory(String::from(colors[1]), engine, true, 0);
     println!(
         "Car order 1: {:?}, Hard top = {}, {:?}, {}, {} miles",
-        car.age.0, car.roof, car.motor, car.color, car.age.1
+        car.age.0,
+        car.roof,
+        car.motor,
+        car.color,
+        car.age.1
     );
 
     // Car order #2: Used, Semi-automatic, Convertible
@@ -76,7 +80,11 @@ fn main() {
     car = car_factory(String::from(colors[2]), engine, false, 100);
     println!(
         "Car order 2: {:?}, Hard top = {}, {:?}, {}, {} miles",
-        car.age.0, car.roof, car.motor, car.color, car.age.1
+        car.age.0,
+        car.roof,
+        car.motor,
+        car.color,
+        car.age.1
     );
 
     // Car order #3: Used, Automatic, Hard top
@@ -84,6 +92,10 @@ fn main() {
     car = car_factory(String::from(colors[3]), engine, true, 200);
     println!(
         "Car order 3: {:?}, Hard top = {}, {:?}, {}, {} miles",
-        car.age.0, car.roof, car.motor, car.color, car.age.1
+        car.age.0,
+        car.roof,
+        car.motor,
+        car.color,
+        car.age.1
     );
 }
